842e03e7364c941e75925715536bb7427bc2d80b
mohammedBouzar <mohammed1.bouzar@gmail.com>
2022-04-15 17:10:47 +0000

    first commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var d = document.getElementById("div_menu");
+var m = document.getElementById("main");
+
+function show(){
+    d.style.display = 'inline';
+    m.style.left = "23%";
+}
+
+function hide(){
+    d.style.display = 'none';
+    m.style.left = "0%";
+}
